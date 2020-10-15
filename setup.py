5da6efda52d4853e0fd7fe342cc24dec22e077d1
KnugiHK <24708955+KnugiHK@users.noreply.github.com>
2020-10-16 00:22:05 +0800

    Delete workers

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rtmplite3", # Replace with your own username
+    name="rtmplite3",
     version="0.3.0",
     author="KnugiHK",
     author_email="info@knugi.xyz",
